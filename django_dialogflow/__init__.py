4a532b2489d058be51521b46c1b296abaf5a9ad9
Mallikarjunarao Kosuri <venkatamallikarjunarao.kosuri@adtran.com>
2019-05-26 23:20:15 +0530

    bump version

--- django_dialogflow/__init__.py
+++ django_dialogflow/__init__.py
@@ -1,7 +1,7 @@
 """
 Django Dialogflow chat agent
 """
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 __author__ = 'Mallikarjunarao Kosuri'
 __email__ = 'malli.kv2@gmail.com'
 __url__ = 'https://github.com/vkosuri/django-dialogflow'
